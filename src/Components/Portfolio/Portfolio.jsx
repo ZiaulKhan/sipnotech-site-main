d5b73d51b7251ba8314c23821d55d326b04e6f5e
ZiaulKhan <ziaul.khan11@gmail.com>
2022-12-03 12:58:28 +0530

    5th commit

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./portfolio.css";
 import Card from "../../Common/Card/Card";
 import data from "../../Data/PortfolioData";
@@ -7,13 +7,35 @@ const Portfolio = (props) => {
   const category = data.map((item) => item.category);
   const uniqueCategory = ["All", ...new Set(category)];
 
+  const [activeCategory, setActiveCategory] = useState("All");
+
+  const onClick = (activeCat) => {
+    setActiveCategory(activeCat);
+  };
+
+  const [project, setProject] = useState(data);
+
+  const displayData = (activeCategory) => {
+    if (activeCategory === "All") {
+      setProject(data);
+      return;
+    } else {
+      const filterProject = data.filter((item) => item.category === category);
+      setProject(filterProject);
+    }
+  };
+  displayData();
   return (
     <div className="sipnotech__portfolio">
       <h1 className="sipnotech__portfolio_heading">{props.heading}</h1>
       <h1 className="sipnotech__portfolio_title">{props.title}</h1>
       <div className="sipnotech__portfolio_categories_container">
         {uniqueCategory.map((item) => (
-          <p key={item.id} className="sipnotech__portfolio_categories">
+          <p
+            key={item.id}
+            onClick={() => onClick(item)}
+            onFilterProjects={displayData()}
+            className="sipnotech__portfolio_categories">
             {item}
           </p>
         ))}
